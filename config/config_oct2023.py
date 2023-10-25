10e1d867a98ed7c6699a42c214d66a1a2bc131f1
mathewhorne <57614046+mathewhorne@users.noreply.github.com>
2023-10-25 16:20:56 +0900

    Other_trans_25Oct2023

---
+++ config/config_oct2023.py
@@ -0,0 +1,183 @@
+# CONFIG script, April 2023
+# Import dependencies
+ 
+import pandas as pd 
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.dates import DateFormatter
+import os
+import datetime
+from datetime import datetime
+import re
+import shutil
+from numpy.core.numeric import NaN
+from openpyxl import Workbook
+import xlsxwriter
+import pandas.io.formats.excel
+from pandas import ExcelWriter
+import requests
+import ssl
+import urllib3
+import xml.etree.ElementTree as ET
+import io
+import typing
+import urllib.request
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import lxml
+import wbgapi as wb
+import json
+import pytest
+import seaborn as sns
+from pandas import json_normalize
+from textwrap import wrap
+import plotly
+import sklearn
+import featuretools as ft
+import math
+import itertools
+from itertools import chain, combinations
+import warnings
+from scipy.interpolate import interp1d, splev, splrep
+
+# Set directory
+os.chdir(re.split('9th_supply_components', os.getcwd())[0] + '9th_supply_components')
+
+# Date
+timestamp = datetime.now().strftime('%Y_%m_%d')
+
+# Set file_date_id here so that if we are running the script alone, versus through integrate, 
+# we can have the variable available
+try:
+    file_date_id
+except NameError:
+    file_date_id = None
+
+USE_LATEST_OUTPUT_DATE_ID = True
+
+# Create option to set file_date_id to the date_id of the latest created output files. 
+# (helpful when producing graphs and analysing output data)
+if USE_LATEST_OUTPUT_DATE_ID == True:
+    list_of_files = glob.glob('./results/*.csv')
+
+if len(list_of_files) > 0: 
+    latest_file = max(list_of_files, key = os.path.getctime)
+    # Get file_date_id using regular expression code as per below. 
+    # Want to grab the firt 8 digits and then an underscore and then the next 4 digits
+    file_date_id = re.search(r'date(\d{8})_(\d{4})', latest_file).group(0)
+else:
+    pass
+
+# Modelling variables
+BASE_YEAR = 2017
+END_YEAR = 2100
+Scenario_list = ['reference', 'target']
+
+# Machine learning
+DEGREE = 2
+LAGS = 1
+KFOLD_SPLIT = 3
+TOP_MODELS = 5
+
+model_output_file_name = 'model_output_years_{}_to_{}_{}.csv'.format(BASE_YEAR, END_YEAR, file_date_id)
+
+# Model concordance file names
+model_concordance_version = file_date_id # Example: '20220824_1256'
+model_concordance_file_name  = 'model_concordance{}.csv'.format(model_concordance_version)
+
+# Analysis variables
+SCENARIO_OF_INTEREST = 'reference'
+
+# Economies (including larger regions)
+variable_path = './data/config/'
+
+economy_list = pd.read_csv(variable_path + 'APEC_economies.csv').iloc[:, 0]
+fuels_list = pd.read_csv(variable_path + 'EGEDA_fuels.csv').iloc[:, 0]
+subfuels_list = pd.read_csv(variable_path + 'EGEDA_subfuels.csv').iloc[:, 0]
+
+# Latest APEC_GDP_data file
+path_to_gdp = '../macro_variables_9th/results/GDP_estimates/data/'
+gdp_prefix = 'APEC_GDP_data_'
+
+gdp_files = glob.glob(path_to_gdp + gdp_prefix + '*.csv')
+
+if len(gdp_files) > 0:
+    latest_gdp = max(gdp_files, key = os.path.getctime)
+    gdp_date = re.search(r'(\d{4})_(\d{2})_(\d{2})', latest_gdp).group(0)
+
+else:
+    pass
+
+# Path to EGEDA energy data
+EGEDA_path = '../Outlook9th_EBT/results/'
+EGEDA_prefix = 'model_df_wide_ref_'
+
+EGEDA_files = glob.glob(EGEDA_path + EGEDA_prefix + '*.csv')
+
+if len(EGEDA_files) > 0:
+    latest_EGEDA = max(EGEDA_files, key = os.path.getctime)
+    EGEDA_date = re.search(r'(\d{4})(\d{2})(\d{2})', latest_EGEDA).group(0)
+
+else: 
+    pass
+
+# Colour palettes
+
+pink_foam = ["#54bebe", "#76c8c8", "#98d1d1", "#badbdb", "#dedad2", "#e4bcad", "#df979e", "#d7658b", "#c80064"]
+salmon_aqua = ["#e27c7c", "#a86464", "#6d4b4b", "#503f3f", "#333333", "#3c4e4b", "#466964", "#599e94", "#6cd4c5"]
+retro_metro = ["#ea5545", "#f46a9b", "#ef9b20", "#edbf33", "#ede15b", "#bdcf32", "#87bc45", "#27aeef", "#b33dc6"]
+orange_purple = ["#ffb400", "#d2980d", "#a57c1b", "#786028", "#363445", "#48446e", "#5e569b", "#776bcd", "#9080ff"]
+dutch_field = ["#e60049", "#0bb4ff", "#50e991", "#e6d800", "#9b19f5", "#ffa300", "#dc0ab4", "#b3d4ff", "#00bfa0"]
+blue_red = ["#1984c5", "#22a7f0", "#63bff0", "#a7d5ed", "#e2e2e2", "#e1a692", "#de6e56", "#e14b31", "#c23728"]
+san_andreas = ['#000000', '#2a77a1', '#840410', '#263739', '#86446e', '#d78e10', '#4c75b7', '#bdbec6', '#58595a', '#335f3f']
+
+custom_palette = {'reference': salmon_aqua[0],
+                  'target': salmon_aqua[-1],
+                  'reference activity': salmon_aqua[0],
+                  'target activity': salmon_aqua[-1],
+                  'reference energy': salmon_aqua[0],
+                  'target energy': salmon_aqua[-1],
+                  'CCS capacity': salmon_aqua[0],
+                  'Non-CCS capacity': salmon_aqua[-1]}
+
+fuel_industry = fuels_list[[0, 1, 5, 6, 7, 11, 14, 15, 16, 17]].reset_index(drop = True)
+
+fuel_custom = pd.concat([fuel_industry[:-2], pd.Series(list(subfuels_list)[-1]), fuel_industry[-2:]]).values.tolist()
+
+fuel_palette1 = {'01_coal': "#e27c7c",
+                 '02_coal_products': "#a86464",
+                 '03_peat': "blue",
+                 '04_peat_products': "blue",
+                 '05_oil_shale_and_oil_sands': "blue",
+                 '06_crude_oil_and_ngl': "#bebebe",
+                 '07_petroleum_products': "#a5cdf0",
+                 '08_gas': "#1e465a",
+                 '09_nuclear': "blue",
+                 '10_hydro': "blue",
+                 '11_geothermal': "#1aa7ec", 
+                 '12_solar': "#1aa7ec",
+                 '13_tide_wave_ocean': "blue",
+                 '14_wind': "blue",
+                 '15_solid_biomass': "#28825a",
+                 '16_others': "#C36517",
+                 '16_x_others': "#C36517",
+                 '16_01_biogas': "#98FB98",
+                 '16_x_hydrogen': "#fce205",
+                 '17_electricity': "#6cd4c5",
+                 '18_heat': "#ea5545"}
+
+fuel_palette_CCS = {'CCS capacity': "#ea5545",
+                    'Non-CCS capacity': "#bebebe"}
+
+tpes_palette = {'01_production': "#1e465a",
+                '02_imports': "#6cd4c5",
+                '03_exports': "#e27c7c",
+                '06_stock_changes': "#a5cdf0"}
+
+coal_palette = {'01_coal': "#e27c7c",
+                '01_01_coking_coal': "#1e465a",
+                '01_05_lignite': "#a5cdf0",
+                '01_x_thermal_coal': "#6cd4c5",
+                '02_coal_products': "#a86464"}
---
+++ workflow/scripts/a1_trans_own_use_addon.py
@@ -0,0 +1,143 @@
+# Modelling 9_transformation and 10_own_use_and_losses that have not been modelled by other sectors
+# Set working directory to be the project folder 
+import os
+import re
+
+wanted_wd = '9th_supply_components'
+os.chdir(re.split(wanted_wd, os.getcwd())[0] + wanted_wd)
+
+# execute config file
+config_file = './config/config_oct2023.py'
+with open(config_file) as infile:
+    exec(infile.read())
+
+# Grab APEC economies (economy_list defined in config file)
+APEC_economies = list(economy_list)[:-7]
+
+# 2021 and beyond
+proj_years = list(range(2021, 2071, 1))
+proj_years_str = [str(i) for i in proj_years]
+
+# latest EGEDA data
+EGEDA_df = pd.read_csv(latest_EGEDA)
+EGEDA_df[EGEDA_df['sub1sectors'].str.startswith('09')]['sub1sectors'].unique()
+
+# sub1sectors transformation categories that need to be modelled
+trans_df = pd.read_csv('./data/config/transformation_orphans.csv', header = None)
+trans_cats = trans_df[0].values.tolist()
+
+# sub2sectors own-use categories that need to be modelled
+ownuse_df = pd.read_csv('./data/config/ownuse_cats.csv', header = None)
+ownuse_cats = ownuse_df[0].values.tolist()
+
+relevant_fuels = ['01_coal', '02_coal_products', '06_crude_oil_and_ngl', '07_petroleum_products',
+                  '08_gas', '15_solid_biomass', '16_others', '17_electricity', '18_heat']
+
+EGEDA_trans = EGEDA_df[EGEDA_df['sub1sectors'].isin(trans_cats)].copy().reset_index(drop = True)
+EGEDA_own = EGEDA_df[EGEDA_df['sub2sectors'].isin(ownuse_cats)].copy().reset_index(drop = True)
+
+for economy in APEC_economies:
+    # Save location
+    save_location = './results/01_trans_own_addon/{}/'.format(economy)
+
+    if not os.path.isdir(save_location):
+        os.makedirs(save_location)
+
+    # This is the location where the merged TFC and transformation results are provided
+    modelled_result = './data/paste_TFC_trans_integration_here/'
+
+    file_prefix = 'merged_file_' + economy
+
+    # Define vector with file names
+    files = glob.glob(modelled_result + file_prefix + '*.csv')
+
+    # Identify the most uptodate file
+    if len(files) > 0:
+        latest_file = max(files, key = os.path.getctime)
+        merged_file_date = re.search(r'(\d{4})(\d{2})(\d{2})', latest_file).group(0)
+
+        results_df = pd.read_csv(latest_file)
+        results_ref = results_df[results_df['scenarios'] == 'reference'].copy().reset_index(drop = True)
+        results_tgt = results_df[results_df['scenarios'] == 'target'].copy().reset_index(drop = True)
+
+        # Transformation results needed
+        EGEDA_trans_ref = EGEDA_trans[EGEDA_trans['economy'] == economy].copy().reset_index(drop = True)
+        EGEDA_trans_tgt = EGEDA_trans[EGEDA_trans['economy'] == economy].copy().reset_index(drop = True)
+        EGEDA_trans_tgt['scenarios'] = 'target'
+
+        # Own-use results needed
+        EGEDA_own_ref = EGEDA_own[EGEDA_own['economy'] == economy].copy().reset_index(drop = True)
+        EGEDA_own_tgt = EGEDA_own[EGEDA_own['economy'] == economy].copy().reset_index(drop = True)
+        EGEDA_own_tgt['scenarios'] = 'target'
+
+        scenario_dict = {'ref': [results_ref, EGEDA_trans_ref, EGEDA_own_ref],
+                         'tgt': [results_tgt, EGEDA_trans_tgt, EGEDA_own_tgt]}
+        
+        for scenario in scenario_dict.keys():
+            # Data frame with results from other sectors to use to build trajectories to fill the trans and own df's
+            tfc_trans_df = scenario_dict[scenario][0]
+            # Subset so only consumption categories are included
+            trans_df = tfc_trans_df[(tfc_trans_df['sectors'].isin(['09_total_transformation_sector'])) &
+                                    (tfc_trans_df['sub1sectors'] == 'x')].copy().reset_index(drop = True)
+            
+            for column in trans_df.columns:
+                if pd.api.types.is_numeric_dtype(trans_df[column]):
+                    trans_df[column] = trans_df[column].abs()
+
+            tfc_df = tfc_trans_df[(tfc_trans_df['sectors'].isin(['12_total_final_consumption'])) &
+                                  (tfc_trans_df['sub1sectors'] == 'x')].copy().reset_index(drop = True)
+            
+            tfc_trans_df = pd.concat([trans_df, tfc_df]).copy().reset_index(drop = True)
+
+            # Subset so its only high level fuels and sum tfc and transformation
+            tfc_trans_df = tfc_trans_df[tfc_trans_df['subfuels'] == 'x']\
+                .groupby(['scenarios', 'economy', 'sub1sectors', 'sub2sectors', 'sub3sectors', 'sub4sectors', 'subfuels', 'fuels'])\
+                    .sum().reset_index().assign(sectors = 'consumption tfc trans')
+            
+            # Now only keep relevant fuels 
+            tfc_trans_df = tfc_trans_df[tfc_trans_df['fuels'].isin(relevant_fuels)].copy().reset_index(drop = True)
+            tfc_trans_df = tfc_trans_df.fillna(0)
+            
+            # Dataframes to populate
+            trans_df = scenario_dict[scenario][1]
+            trans_df = trans_df.fillna(0)
+            own_df = scenario_dict[scenario][2]
+            own_df = own_df.fillna(0)
+
+            # Define results dataframe
+            trans_results_df = pd.DataFrame(columns = trans_df.columns)
+            own_results_df = pd.DataFrame(columns = own_df.columns)
+
+            for fuel in tfc_trans_df['fuels'].unique():
+                fuel_agg_row = tfc_trans_df[tfc_trans_df['fuels'] == fuel].copy().reset_index(drop = True)
+
+                trans_results_interim = trans_df[trans_df['fuels'] == fuel].copy().reset_index(drop = True)
+                own_results_interim = own_df[own_df['fuels'] == fuel].copy().reset_index(drop = True)
+
+                for year in proj_years_str:
+                    if fuel_agg_row.loc[0, str(int(year) - 1)] == 0:
+                        ratio = 0
+
+                    else:
+                        ratio = fuel_agg_row.loc[0, year] / fuel_agg_row.loc[0, str(int(year) - 1)]
+                    
+                    # Now populate the transformation and own use dataframe based on the growth ratio calculated above
+                    for row in trans_results_interim.index:
+                        trans_results_interim.loc[row, year] = trans_results_interim.loc[row, str(int(year) - 1)] * ratio
+
+                    for row in own_results_interim.index:
+                        own_results_interim.loc[row, year] = own_results_interim.loc[row, str(int(year) - 1)] * ratio
+
+                if trans_results_df.empty:
+                    trans_results_df = trans_results_interim.copy()
+                else:             
+                    trans_results_df = pd.concat([trans_results_df, trans_results_interim]).copy().reset_index(drop = True)
+                
+                if own_results_df.empty:
+                    own_results_df = own_results_interim.copy()
+                else:
+                    own_results_df = pd.concat([own_results_df, own_results_interim]).copy().reset_index(drop = True)
+
+            trans_results_df.to_csv(save_location + economy + '_other_transformation_' + scenario + '_' + timestamp + '.csv', index = False)
+            own_results_df.to_csv(save_location + economy + '_other_own_use_' + scenario + '_' + timestamp + '.csv', index = False)
+
